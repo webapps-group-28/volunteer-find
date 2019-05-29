a6fef72494040d736cad6d8ada759041035f5280
Zimu Zeng <zimu.zeng@gmail.com>
2019-05-29 16:56:25 +0100

    Migrated Volunteer model to database

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from . import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
@@ -10,14 +9,14 @@ class Project(models.Model):
     type = models.CharField(max_length=100)
     duration = models.FloatField()
     date = models.DateField(auto_now_add=True)
-    organiser = models.ForeignKey(User, default=None)
+    organiser = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.title
 
 class Volunteer(models.Model):
-    user = models.ForeignKey(User)
-    project = models.ForeignKey(models.Project)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name + " " + self.project.title
