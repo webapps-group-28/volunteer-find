e1c5d06678f284418d5a04396f1f07c3d59aa688
Zimu Zeng <zimu.zeng@gmail.com>
2019-05-29 16:27:08 +0100

    Migrated Project model to database

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Project(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     description = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-    type = models.CharField()
+    type = models.CharField(max_length=100)
     duration = models.FloatField()
     date = models.DateField(auto_now_add=True)
