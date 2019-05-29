01db18139813e8b7d10a701b89fa3017f5dff897
Zimu Zeng <zimu.zeng@gmail.com>
2019-05-29 16:53:09 +0100

    Added Volunteer model

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from . import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
@@ -8,6 +10,14 @@ class Project(models.Model):
     type = models.CharField(max_length=100)
     duration = models.FloatField()
     date = models.DateField(auto_now_add=True)
+    organiser = models.ForeignKey(User, default=None)
 
     def __str__(self):
         return self.title
+
+class Volunteer(models.Model):
+    user = models.ForeignKey(User)
+    project = models.ForeignKey(models.Project)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name + " " + self.project.title
