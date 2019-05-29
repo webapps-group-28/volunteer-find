d61219e644159881eb1ed4e462966f68b4ff41f1
Zimu Zeng <zimu.zeng@gmail.com>
2019-05-29 22:56:43 +0100

    Fixed incorrect import statement in urls.py

--- volunteerfind/urls.py
+++ volunteerfind/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
 from . import views
-from projects import projects_views
+from projects import views as project_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
