c2e6b7fb1d4b8761734067e4a37f9a395e5c15b4
Zimu Zeng <zimu.zeng@gmail.com>
2019-05-29 19:23:47 +0100

    Added onsubmit event

--- static/search.js
+++ static/search.js
@@ -1,11 +1,14 @@
-var latInput = document.getElementById("latInput");
-var longInput = document.getElementById("longInput");
+var searchForm = document.getElementById("searchForm");
+searchForm.onsubmit = function() {
+  var latInput = document.getElementById("latInput");
+  var longInput = document.getElementById("longInput");
 
-function getCurrentLocation() {
-  navigator.geolocation.getCurrentPosition(setPosition);
-}
+  function getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(setPosition);
+  }
 
-function setPosition(position) {
-  latInput.value = position.coords.latitude;
-  longInput.value = position.coords.longitude;
+  function setPosition(position) {
+    latInput.value = position.coords.latitude;
+    longInput.value = position.coords.longitude;
+  }
 }
