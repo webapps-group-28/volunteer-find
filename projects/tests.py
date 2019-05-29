ff4a61106c426c517c4fa579148c89e8092337ed
Zimu Zeng <zimu.zeng@gmail.com>
2019-05-29 18:37:03 +0100

    Added unit tests for location filtering

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from . import models
+from . import views
 
-# Create your tests here.
+class ProjectSearchTestCase(TestCase):
+    projects = []
+
+    def setUp(self):
+        project = models.Project()
+        project.title = "Clean up Hyde Park"
+        project.description = "Contribute towards the clean up of the Hyde Park, removing litter."
+        project.latitude = 51.507568
+        project.longitude = -0.166409
+        project.type = "Environment"
+        project.duration = 15.0
+
+        projects.append(project)
+        project.title = "Rebuild houses destroyed in natural disasters"
+        project.description = "Recent earthquakes have destroyed homes."
+        project.latitude = 39.849393
+        project.longitude = 116.437558
+        project.type = "Building"
+        project.duration = 50
+
+        projects.append(project)
+        project.title = "Help locals in Africa"
+        project.description = "Help them improve local infrastructure"
+        project.latitude = -3.815922
+        project.longitude = 20.380195
+        project.type = "Infrastructure"
+        project.duration = 45
+
+        projects.append(project)
+        project.title = "Teaching a language"
+        project.description = "Help young children learn a new foreign language."
+        project.latitude = 51.511241
+        project.longitude = -0.109625
+        project.type = "Education"
+        project.duration = 10
+
+    def testLocationFiltering(self):
+        output = views.filter_projects(projects, None, None, 1, 51.507568, -0.166409)
+        self.assertTrue(projects[0] in output)
+        self.assertFalse(projects[1] in output)
+        self.assertFalse(projects[2] in output)
+        self.assertFalse(projects[3] in output)
+
+        output = views.filter_projects(projects, None, None, 1000000, 40, 0)
+        self.assertTrue(projects[0] in output)
+        self.assertTrue(projects[1] in output)
+        self.assertTrue(projects[2] in output)
+        self.assertTrue(projects[3] in output)
--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,25 @@ from django.http import HttpResponse
 import geopy.distance
 from . import models
 
-def home(request):
-    return HttpResponse("this is the project page")
+def filter_projects(projects, minduration, maxduration, maxdistance, latitude, longitude):
+    output = []
+    for project in projects:
+        if minduration != None and project.duration < minduration:
+            continue
+        if maxduration != None and project.duration > maxduration:
+            continue
+
+        if latitude != None and maxdistance != None:
+            user_location = (latitude, longitude)
+            project_location = (project.latitude, project.longitude)
+            distance = geopy.distance.distance(user_location, project_location).km
+
+            if distance > maxdistance:
+                continue
+
+        output.append(project)
+    return output
+
 
 def project_search(request):
     projects = models.Project.objects.all()
@@ -27,21 +44,6 @@ def project_search(request):
         latitude = float(request.GET["latitude"])
         longitude = float(request.GET["longitude"])
 
-    output = []
-    for project in projects:
-        if minduration != None and project.duration < minduration:
-            continue
-        if maxduration != None and project.duration > maxduration:
-            continue
-
-        if latitude != None and maxdistance != None:
-            user_location = (latitude, longitude)
-            project_location = (project.latitude, project.longitude)
-            distance = geopy.distance.distance(user_location, project_location).km
-
-            if distance > maxdistance:
-                continue
-
-        output.append(project)
+    output = filter_projects(projects, minduration, maxduration, maxdistance, latitude, longitude)
 
     return render(request, "projects/search.html", { "projects": output })
