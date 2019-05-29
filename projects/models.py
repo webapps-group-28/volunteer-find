8d509629a314c2f4526a72e847ec5f53ebbcc3fc
Zimu Zeng <zimu.zeng@gmail.com>
2019-05-29 16:21:33 +0100

    Created Project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+ Title
+-          Description
+-          One organiser
+-          Location
+-          Type of volunteering
+-          Duration
+-          Date
+
+class Project(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    type = models.CharField()
+    duration = models.FloatField()
+    date = models.DateField(auto_now_add=True)
