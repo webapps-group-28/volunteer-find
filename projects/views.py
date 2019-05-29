981ef80e51ae6021af7fbd8dbc91d09d4a5af0a9
Zimu Zeng <zimu.zeng@gmail.com>
2019-05-29 17:41:55 +0100

    Added duration filtering to search API

--- projects/views.py
+++ projects/views.py
@@ -7,4 +7,19 @@ def home(request):
 
 def project_search(request):
     projects = models.Project.objects.all()
-    return render(request, "projects/search.html", { "projects": projects })
+    if minduration in request.GET:
+        minduration = request.GET.minduration
+    else:
+        minduration = 0
+
+    if maxduration in request.GET:
+        maxduration = request.GET.maxduration
+    else:
+        maxduration = 1000
+
+    output = []
+    for project in projects:
+        if project.duration >= minduration and project.duration <= maxduration:
+            output.append(project)
+
+    return render(request, "projects/search.html", { "projects": output })
