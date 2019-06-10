07e8926b870ee514ca92f7c220054b085e9d2839
Zimu Zeng <zimu.zeng@gmail.com>
2019-06-10 12:28:25 +0100

    Debug output

--- static/main.js
+++ static/main.js
@@ -1,9 +1,10 @@
 buttons = document.getElementsByClassName("signup-button");
 for (var button in buttons) {
+  console.log("jesus");
   button.onclick = function() {
     var http = new XMLHttpRequest();
     http.open("POST", "/projects/signup/", true);
     var params = "project_id=" + button.id.substring(7, button.id.length);
     http.send(params);
-  }
+  };
 }
