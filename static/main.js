1eef10e0058eee80731345a282761c49c6e57563
Zimu Zeng <zimu.zeng@gmail.com>
2019-06-10 12:39:20 +0100

    Fixed error with closures

--- static/main.js
+++ static/main.js
@@ -1,9 +1,13 @@
 buttons = document.getElementsByClassName("signup-button");
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].onclick = function() {
-    var http = new XMLHttpRequest();
-    http.open("POST", "/projects/signup/", true);
-    var params = "project_id=" + buttons[i].id.substring(7, buttons[i].id.length);
-    http.send(params);
+    sendSignupRequest(i);
   };
+}
+
+function sendSignupRequest(i) {
+  var http = new XMLHttpRequest();
+  http.open("POST", "/projects/signup/", true);
+  var params = "project_id=" + buttons[i].id.substring(7, buttons[i].id.length);
+  http.send(params);
 }
