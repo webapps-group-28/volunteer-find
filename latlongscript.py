a0da4d25b6db70e6d7e3f4832b9cba151aa0a3e4
Prem Chowdhry <prem@Tests-MacBook-Pro.local>
2019-06-13 12:56:27 +0100

    Fixed data retrieval

--- latlongscript.py
+++ latlongscript.py
@@ -2,7 +2,7 @@ import postcodes_io_api
 
 def lat_long_from_postcode(postcode):
     api  = postcodes_io_api.Api(debug_http=True)
-    data = api.get_postcode(postcode)
+    data = api.get_postcode(postcode)['result']
     lat = data['latitude']
     lng = data['longitude']
     return lat, lng
