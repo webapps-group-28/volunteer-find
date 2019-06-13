f51aef060cbb777cf29c75c2851a750df8673f9e
Prem Chowdhry <prem@Tests-MacBook-Pro.local>
2019-06-13 12:49:45 +0100

    Added script to return lat and log form postcode. Also added the api to the pip requirements

---
+++ latlongscript.py
@@ -0,0 +1,8 @@
+import postcodes_io_api
+
+def lat_long_from_postcode(postcode):
+    api  = postcodes_io_api.Api(debug_http=True)
+    data = api.get_postcode(postcode)
+    lat = data['latitude']
+    lng = data['longitude']
+    return lat, lng
